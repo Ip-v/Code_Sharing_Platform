bedc537da5d4c5d4863ba5e96fbc54894bed2156
Ip-v <recdisc@yandex.ru>
2020-12-13 20:37:33 +0300

    Initial commit

---
+++ Code Sharing Platform/task/src/platform/CodeSharingPlatform.java
@@ -0,0 +1,36 @@
+package platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CodeSharingPlatform {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSharingPlatform.class, args);
+    }
+
+    @GetMapping(path = "/code")
+        public String html() {
+        return "<html>\n" +
+                "<head>\n" +
+                "    <title>Code</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <pre>\n" +
+                "public static void main(String[] args) {\n" +
+                "    SpringApplication.run(CodeSharingPlatform.class, args);\n" +
+                "}</pre>\n" +
+                "</body>\n" +
+                "</html>";
+    }
+
+    @GetMapping(path = "/api/code")
+    public String api() {
+        return "{\"code\": \"public static void main(String[] args) {\\n    SpringApplication.run(CodeSharingPlatform.class, args);\\n}\"}";
+    }
+
+}
